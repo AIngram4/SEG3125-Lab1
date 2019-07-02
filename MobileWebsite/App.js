e7b003bd2e858014f46b9603a5d15be4c7b3cc56
Aidan <aboas071@uottawa.ca>
2019-07-01 22:27:03 -0400

    Home page first tweaks

--- MobileWebsite/App.js
+++ MobileWebsite/App.js
@@ -39,14 +39,14 @@ import CheckoutScreen from "./components/checkout/CheckoutScreen";
 import HomeScreen from "./components/home/HomeScreen";
 
 const RootStack = createStackNavigator({
+    Home: {
+        screen: HomeScreen,
+    },
     Cart: {
         screen: CartScreen,
     },
     Restaurants: {
         screen: RestaurantScreen,
-    },
-    Home: {
-        screen: HomeScreen,
     },
     Menu: {
         screen: MenuScreen,
--- MobileWebsite/components/home/HomeScreen.js
+++ MobileWebsite/components/home/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, AppRegistry, ScrollView} from 'react-native';
+import {Text, StyleSheet, View, AppRegistry, ScrollView} from 'react-native';
 import {SearchBar} from "react-native-elements";
+import {Dropdown} from 'react-native-material-dropdown';
 
 import NavBar from '../navigation/navBar';
 
@@ -11,10 +12,40 @@ export default class HomeScreen extends Component {
     };
 
     render() {
+      let data = [{
+        value: 'Breakfast',
+      }, {
+        value: 'Lunch',
+      }, {
+        value: 'Dinner',
+      }, {
+        value: 'Other',
+      }];
         return (
             <View style={styles.container}>
-                {/*Use a scroll view to make sure the nav bar gets forced to bottom*/}
-                <SearchBar placeholder="Type here..."/>
+              <ScrollView style={styles.scrollContainer}>
+                <Text style={styles.titleText}>Welcome to Order It!</Text>
+                <SearchBar
+                containerStyle={{
+                  backgroundColor: 'transparent',
+                  borderBottomWidth: 0,
+                  borderTopWidth: 0,
+                }}
+                round
+                placeholder="Search For Restaurants"/>
+                <SearchBar
+                round
+                containerStyle={{
+                  backgroundColor: 'transparent',
+                  borderBottomWidth: 0,
+                  borderTopWidth: 0,
+                }}
+                placeholder="Search for City"/>
+
+                <Dropdown
+                  label='Select Meal Type'
+                  data={data} />
+                </ScrollView>
                 <NavBar navigation={this.props.navigation}/>
             </View>
         );
@@ -24,6 +55,14 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    titleText: {
+        marginLeft: 20,
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 28,
+        marginBottom: 10,
+        marginTop: 25,
     },
 });
 
